617871ee75f35cf86d115945c392abca46683354
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2023-02-07 23:39:14 -0700

    update server.c due to the new C API

--- c_interface/hello_world/server.c
+++ c_interface/hello_world/server.c
@@ -1,8 +1,9 @@
 #include "common.h"
 
 // req_handle is the object that caller create and fill in data
+uint8_t rpc_id = 0;
 void (req_func)(void *req_handle, void *context) {
-	erpc_req_response_enqueue(req_handle, "hello world", kMsgSize);
+	erpc_req_response_enqueue(rpc_id, req_handle, "hello world", kMsgSize);
 }
 
 int main() {
@@ -12,7 +13,7 @@ int main() {
   erpc_init(server_uri, 0, 0);
 
   erpc_register_req_func(kReqType, req_func, 0);
-  erpc_start(NULL, 0, NULL, 0); 
+  erpc_start(NULL, rpc_id, NULL, 0); 
 
-  erpc_run_event_loop(100000);
+  erpc_run_event_loop(rpc_id, 100000);
 }
