e04e7c5f025a3153824ac557fed3bac3c06625c6
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2023-03-25 01:04:50 -0600

    upload openloop_test_wrapper.sh and openloop_test.sh

---
+++ my_tests/openloop_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+function usage {
+        echo "$0 [fib number] [listener count]"
+        exit 1
+}
+
+if [ $# != 2 ] ; then
+        usage
+        exit 1;
+fi
+
+fib_num=$1
+listener_count=$2
+
+first_worker_core_id=$((1 + $listener_count))
+
+chmod 400 ./id_rsa
+
+remote_ip="128.110.219.2"
+
+#cores_list=(1 2 4 6 8 10 12 14 16 18 20 24 28 32 36 40 44 48 52 56 60 64 68 70)
+#cores_list=(1 2 4 6 8 10 12 14 16 18 20 22 24)
+cores_list=(10)
+ulimit -n 655350
+
+
+path="/my_mount/mem_log/sledge-serverless-framework/runtime/tests"
+for(( i=0;i<${#cores_list[@]};i++ )) do
+        server_log="server-"${cores_list[i]}"-$fib_num.log"
+        ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "sudo $path/start.sh ${cores_list[i]} $listener_count $first_worker_core_id > $server_log 2>&1 &"
+        echo "sledge start with worker core ${cores_list[i]}"
+        pushd ../ 
+	#scripts/do.sh 1 0 > client.txt
+	scripts/do.sh 1 0 
+	popd
+        ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip  "sudo $path/kill_sledge.sh"
+	sleep 10 
+done
+folder_name="$fib_num""_$listener_count"
+ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip  "mkdir $path/$folder_name"
+ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip  "mv *.log $path/$folder_name"
+
---
+++ my_tests/openloop_test_wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function usage {
+        echo "$0 [max listener count]"
+        exit 1
+}
+
+if [ $# != 1 ] ; then
+        usage
+        exit 1;
+fi
+
+max_listener_count=$1
+
+#for ((i=1; i<=$max_listener_count; i=i+1))
+for ((i=$max_listener_count; i<=$max_listener_count; i=i+1))
+do
+        new_line="--num_server_threads ${i}"
+        sed -i "3c ${new_line}" ../apps/server_rate/config
+        ./openloop_test.sh 15 $i
+done
+
