c95b05935eacdc4eb72760b7370989a3e018aca1
xiaosuGW <xiaosuGW@localhost>
2023-05-20 18:22:03 -0600

    update script and config files

--- my_tests/openloop_test.sh
+++ my_tests/openloop_test.sh
@@ -16,7 +16,7 @@ first_worker_core_id=$((1 + $listener_count))
 
 chmod 400 ./id_rsa
 
-remote_ip="128.110.218.246"
+remote_ip="128.110.218.243"
 
 #cores_list=(1 2 4 6 8 10 12 14 16 18 20 24 28 32 36 40 44 48 52 56 60 64 68 70)
 #cores_list=(1 2 4 6 8 10 12 14 16 18 20 22 24)
@@ -26,6 +26,7 @@ ulimit -n 655350
 
 #path="/my_mount/sledge-serverless-framework/runtime/tests"
 path="/my_mount/old_version/sledge-serverless-framework/runtime/tests"
+#path="/my_mount/edf_interrupt/sledge-serverless-framework/runtime/tests"
 for(( i=0;i<${#cores_list[@]};i++ )) do
         server_log="server-"${cores_list[i]}"-$fib_num.log"
         ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "sudo $path/start.sh ${cores_list[i]} $listener_count $first_worker_core_id > $server_log 2>&1 &"
