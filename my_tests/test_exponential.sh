22fa233612c99a0e0cb61a2fc4e2d37948f174d9
Xiaosu Lyu <lyuxiaosu@gwu.edu>
2024-06-05 00:32:54 -0600

    update test_log_normal.sh and test_exponential.sh

--- my_tests/test_exponential.sh
+++ my_tests/test_exponential.sh
@@ -21,16 +21,29 @@ dispatcher_policy=$1
 disable_busy_loop=$2
 disable_autoscaling=$3
 worker_count=$4
+json_file="hash.json"
+server_workers=6
+path="/my_mount/sledge-serverless-framework/runtime/tests"
 
 flag="exponential-$worker_count"
 base_throughput=4000
 
 if [ "$dispatcher_policy" == "SHINJUKU" ]; then
-    throughput_percentage=(1 10 20 30 40 50 60 70 80 82 84 86 88 90) 
+    sed -i 's/--is_darc=[^ ]*/--is_darc=false/g' /my_mount/eRPC/apps/openloop_exponential/config
+    #throughput_percentage=(1 10 20 30 40 50 60 70 80 90 100 104 108 110 112 114 116) 
+    throughput_percentage=(10 20) 
+elif [ "$dispatcher_policy" == "DARC" ]; then
+    sed -i 's/--is_darc=[^ ]*/--is_darc=true/g' /my_mount/eRPC/apps/openloop_exponential/config
+    throughput_percentage=(1 10 20 30 40 50 60 62 64 68 70 72 74 76 78 80)
+    #throughput_percentage=(80)
+    json_file="hash_darc.json"
+    reserve_workers=$(($server_workers / 2))
+    ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "$path/sed_json.sh $path/$json_file $reserve_workers $reserve_workers"
 else
-    throughput_percentage=(1 10 20 30 40 50 60 70 80 90 91 92 93 94 95 96 98 100 102 104 106)
+    sed -i 's/--is_darc=[^ ]*/--is_darc=false/g' /my_mount/eRPC/apps/openloop_exponential/config
+    throughput_percentage=(1 10 20 30 40 50 60 70 80 90 100 110 116 118 120 122 124 126)
 fi
- 
+
 #throughput_percentage=(10 20 30 40 50 60)
 #throughput_percentage=(1 10 20 30 40 50 60 70 80 90 91 92 93 94 95 96)
 #for shinjuku 15
@@ -40,7 +53,6 @@ fi
 #throughput_percentage=(1 10 20 30 40 50 60 70 80 90 91 92 93 94 95 96)
 #throughput_percentage=(97 98 100)
 
-path="/my_mount/sledge-serverless-framework/runtime/tests"
 #path="/my_mount/old_version/sledge-serverless-framework/runtime/tests"
 #path="/my_mount/edf_interrupt/sledge-serverless-framework/runtime/tests"
 
@@ -65,8 +77,8 @@ for(( i=0;i<${#throughput_percentage[@]};i++ )) do
   		replacement_rps="${replacement_rps},${per_throughput}"
 	done
         replacement_rps="--rps ${replacement_rps}"
+        sed -i "s/^--rps.*/$replacement_rps/" /my_mount/eRPC/apps/openloop_exponential/config
 	echo $replacement_rps
-	./set_rps.sh /my_mount/eRPC/apps/openloop_exponential/config "$replacement_rps" 	
         total_throughput=$(echo "($base_throughput * $worker_count * ${throughput_percentage[i]} / 100)" | bc)
 	server_log="server-${total_throughput}-${throughput_percentage[i]}.log"
 	client_log="client-${total_throughput}-${throughput_percentage[i]}.log"
@@ -75,7 +87,7 @@ for(( i=0;i<${#throughput_percentage[@]};i++ )) do
         echo "start server for $dispatcher_policy ${throughput_percentage[i]} testing..."
 	echo "start $dispatcher_policy ${throughput_percentage[i]} testing..."
         #ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "likwid-powermeter sudo $path/start_test.sh $worker_count 3 5 $dispatcher_policy  $server_log $disable_busy_loop $disable_autoscaling > $cpu_log 2>&1 &"
-        ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "sudo $path/start_test.sh 5 1 3 $dispatcher_policy  $server_log $disable_busy_loop $disable_autoscaling false hash.json > $cpu_log 2>&1 &"
+        ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "sudo $path/start_test.sh $server_workers 1 3 $dispatcher_policy  $server_log $disable_busy_loop $disable_autoscaling false $json_file > $cpu_log 2>&1 &"
 	#echo "start cpu monitoring"
 	#ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "$path/start_monitor.sh $cpu_log > /dev/null 2>&1 &"
 	sleep 10
--- my_tests/test_log_normal.sh
+++ my_tests/test_log_normal.sh
@@ -10,7 +10,7 @@ if [ $# != 4 ] ; then
 fi
 
 chmod 400 ./id_rsa
-remote_ip="128.110.219.0"
+remote_ip="128.110.219.9"
 
 echo "openloop_log_normal" > ../scripts/autorun_app_file
 pushd ../
@@ -21,9 +21,29 @@ dispatcher_policy=$1
 disable_busy_loop=$2
 disable_autoscaling=$3
 worker_count=$4
+server_workers=6
+json_file="hash.json"
+path="/my_mount/sledge-serverless-framework/runtime/tests"
 
 flag="log-normal-$worker_count"
 base_throughput=100
+
+if [ "$dispatcher_policy" == "SHINJUKU" ]; then
+    sed -i 's/--is_darc=[^ ]*/--is_darc=false/g' /my_mount/eRPC/apps/openloop_log_normal/config
+    #throughput_percentage=(1 5 10 15 20 25 35 40 42 44 46 48 50 52 54 56 58 60)
+    throughput_percentage=(10)
+elif [ "$dispatcher_policy" == "DARC" ]; then
+    sed -i 's/--is_darc=[^ ]*/--is_darc=true/g' /my_mount/eRPC/apps/openloop_log_normal/config
+    throughput_percentage=(1 5 10 15 20 25 35 40 42 44 46 48 50 52 54 56 58 60)
+    #throughput_percentage=(1)
+    json_file="hash_darc.json"
+    reserve_workers=$(($server_workers / 2))
+    ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "$path/sed_json.sh $path/$json_file $reserve_workers $reserve_workers"
+else
+    sed -i 's/--is_darc=[^ ]*/--is_darc=false/g' /my_mount/eRPC/apps/openloop_log_normal/config
+    #throughput_percentage=(1 5 10 15 20 25 35 40 42 44 46 48 50 52 54 56 58 60)
+    throughput_percentage=(62 64 66 68 70 72 74 76)
+fi
 
 #throughput_percentage=(10 20 30 40 50 60)
 #throughput_percentage=(1 5 10 15)
@@ -32,14 +52,13 @@ base_throughput=100
 #throughput_percentage=(44 46 48 50 52)
 #for shinjuku
 #throughput_percentage=(1 5 10 15 20 21 22 23 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54)
-throughput_percentage=(1 10 20 30 40 50 60 70 80 90 100 110 120 130 140 142 143 144 145 146 147 148)
+#throughput_percentage=(1 10 20 30 40 50 60 70 80 90 100 110 120 130 140 142 143 144 145 146 147 148)
 
 
 #for sledge 
 #throughput_percentage=(1 5 10 15 20 25 30 35 40 41 42 43 44 46 48 50)
 #throughput_percentage=(55 56 57 58)
 
-path="/my_mount/sledge-serverless-framework/runtime/tests"
 #path="/my_mount/old_version/sledge-serverless-framework/runtime/tests"
 #path="/my_mount/edf_interrupt/sledge-serverless-framework/runtime/tests"
 
@@ -64,7 +83,7 @@ for(( i=0;i<${#throughput_percentage[@]};i++ )) do
 	done
 	replacement_rps="--rps ${replacement_rps}"
 	echo $replacement_rps
-	./set_rps.sh /my_mount/eRPC/apps/openloop_log_normal/config "$replacement_rps" 	
+        sed -i "s/^--rps.*/$replacement_rps/" /my_mount/eRPC/apps/openloop_log_normal/config
 	total_throughput=$(echo "($base_throughput * $worker_count * ${throughput_percentage[i]} / 100)" | bc)
         server_log="server-${total_throughput}-${throughput_percentage[i]}.log"
 	client_log="client-${total_throughput}-${throughput_percentage[i]}.log"
@@ -72,7 +91,7 @@ for(( i=0;i<${#throughput_percentage[@]};i++ )) do
         echo "start server for $dispatcher_policy ${throughput_percentage[i]} testing..."
 	echo "start $dispatcher_policy ${throughput_percentage[i]} testing..."
         #ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "likwid-powermeter sudo $path/start_test.sh $worker_count 3 5 $dispatcher_policy  $server_log $disable_busy_loop $disable_autoscaling > $cpu_log 2>&1 &"
-        ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "sudo $path/start_test.sh 5 1 3 $dispatcher_policy  $server_log $disable_busy_loop $disable_autoscaling "false" "hash.json" > $cpu_log 2>&1 &"
+        ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "sudo $path/start_test.sh $server_workers 1 3 $dispatcher_policy  $server_log $disable_busy_loop $disable_autoscaling false $json_file > $cpu_log 2>&1 &"
 	#echo "start cpu monitoring"
 	#ssh -o stricthostkeychecking=no -i ./id_rsa xiaosuGW@$remote_ip "$path/start_monitor.sh $cpu_log > /dev/null 2>&1 &"
 	sleep 10
